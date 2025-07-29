f6c5c01515f8a6262ce37d433df6906bdabf2ce4
MaitreyeeCoolkarni <dunkul@gmail.com>
2025-07-29 15:48:16 +0530

    Update README.m

--- README.m
+++ README.m
@@ -1,70 +1,71 @@
-# Exp5-if-else-
-## Aim-To learn about switch case and if else conditions
+# Exp5 - If-Else
 
-##Tools-VS Code / Programiz
+## Aim  
+To learn about switch case and if else conditions.
 
-## Algorithm:
+## Tools  
+- VS Code  
+- Programiz
 
-Program 1: Even or Odd Number Checker
-Start: Begin program execution.
+## Algorithm
 
-Declare Variable: Create an integer variable num to store the user’s input.
+### Program 1: Even or Odd Number Checker  
+Start: Begin program execution.  
+Declare Variable: Create an integer variable `num` to store the user’s input.  
+Prompt User: Display the message `"Enter an integer to check for even or odd"` to request input.  
+Read Input: Accept an integer from the user and store it in `num`.
 
-Prompt User: Display the message "Enter a integer to check for even or odd" to request input.
-
-Read Input: Accept an integer from the user and store it in num.
-
-Program 2: Vowel or Consonant Checker
-This program checks whether the entered character is a vowel or a consonant. It compares the character against both uppercase and lowercase vowels (A, E, I, O, U, a, e, i, o, u) using logical OR (||) operators. Any other alphabetic character is considered a consonant.
-1. Input a character (char1)
-2. If char1 is 'a', 'e', 'i', 'o', 'u' (lowercase) or 'A', 'E', 'I', 'O', 'U' (uppercase):
-   - Output "vowel"
-3. Else:
-   - Output "consonant"
+### Program 2: Vowel or Consonant Checker  
+This program checks whether the entered character is a vowel or a consonant. It compares the character against both uppercase and lowercase vowels (`A, E, I, O, U, a, e, i, o, u`) using logical OR (`||`) operators. Any other alphabetic character is considered a consonant.  
+1. Input a character (`char1`)  
+2. If `char1` is `'a', 'e', 'i', 'o', 'u'` (lowercase) or `'A', 'E', 'I', 'O', 'U'` (uppercase):  
+   - Output "vowel"  
+3. Else:  
+   - Output "consonant"  
 4. End
 
+### Program 3: Finding the Largest of Three Numbers  
+This program compares three numbers and determines the largest one using nested if statements. It performs sequential comparisons between all three inputs to identify the maximum.  
+1. Input three integers (`num1`, `num2`, `num3`)  
+2. If `num1` is greater than `num2` and `num1` is greater than `num3`:  
+   - Output "num1 is bigger"  
+3. Else if `num2` is greater than `num3` and `num2` is greater than `num1`:  
+   - Output "num2 is bigger"  
+4. Else:  
+   - Output "num3 is bigger"  
+5. End
 
-Program 3: Finding the Largest of Three Numbers
-This program compares three numbers and determines the largest one using nested if statements. It performs sequential comparisons between all three inputs to identify the maximum.
-1. Input three integers (num1, num2, num3)
-2. If num1 is greater than num2 and num1 is greater than num3:
-   - Output "num1 is bigger"
-3. Else if num2 is greater than num3 and num2 is greater than num1:
-   - Output "num2 is bigger"
-4. Else:
-   - Output "num3 is bigger"
+### Program 4: Game Menu using Switch Case  
+Switch case has been used in this case.  
+Switch case allows multiple options using case.  
+Break is also used.  
+Default case can be used.  
+1. Display menu: A: Shoot, B: Kill, C: Respawn, D: Bomb  
+2. Input a character (`choice`)  
+3. Switch `choice`:  
+   - Case `'A'`: Output "Shoot", break  
+   - Case `'B'`: Output "Kill", break (Note: Code has a bug; break is misplaced)  
+   - Case `'C'`: Output "Respawn", break  
+   - Case `'D'`: Output "Bomb", break  
+4. End
+
+### Program 5: Calculator using Switch Case  
+Same switch case is used.  
+Cases include addition, subtraction, multiplication and division.  
+Input will be taken from the user and the case which matches will be used.  
+1. Display menu: A: Add, B: Subtract, C: Divide, D: Multiply  
+2. Input a character (`choice`)  
+3. Input two numbers (`a`, `b`)  
+4. Switch `choice`:  
+   - Case `'A'`: Output `a + b`, break  
+   - Case `'B'`: Output `a - b`, break (Note: Code has a bug; break is misplaced)  
+   - Case `'C'`: Output `a / b`, break  
+   - Case `'D'`: Output `a * b`, break  
 5. End
 
 
 
-Program 4:
-Switch case has been used in this case
-Switch case allows multiple options using case
-Break is also used
-Default case can be used
-1. Display menu: A:Shoot, B:Kill, C:Respawn, D:Bomb
-2. Input a character (choice)
-3. Switch choice:
-   - Case 'A': Output "Shoot", break
-   - Case 'B': Output "Kill", break (Note: Code has a bug; break is misplaced)
-   - Case 'C': Output "Respawn", break
-   - Case 'D': Output "Bomb", break
-4. End
 
-
-Program 5:
-Same switch case is used
-Cases include addition subtraction multiplication and division
-Input will be taken from the user and the case which matches will be used
-1. Display menu: A:Add, B:Subtract, C:Divide, D:Multiply
-2. Input a character (choice)
-3. Input two numbers (a, b)
-4. Switch choice:
-   - Case 'A': Output a + b, break
-   - Case 'B': Output a - b, break (Note: Code has a bug; break is misplaced)
-   - Case 'C': Output a / b, break
-   - Case 'D': Output a * b, break
-5. End
 
 ##THEORY:
 SWITCH CASE:The switch-case construct is a control flow statement used in many programming languages (like C, C++, Java, etc.) to execute one of several possible blocks of code based on the value of an expression. It provides a cleaner and more efficient alternative to multiple if-else statements when comparing a single variable against multiple possible values.
